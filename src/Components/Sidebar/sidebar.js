d74b88ffadae8b26b0fc30fe9e4c25163b258339
andcarter95 <and.carter95@gmail.com>
2023-02-23 00:09:07 -0600

    Sidebar is completed other than the logo

--- src/Components/Sidebar/sidebar.js
+++ src/Components/Sidebar/sidebar.js
@@ -1,12 +1,41 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './sidebar.scss'
+import LogoA from '../../Assets/Images/PlaceHolderA.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => {
     
 
     return (
         <div className='nav-bar'>
-            <Link className='logo' to='/'></Link>
+            <Link className='logo' to='/'>
+                <img src={LogoA} alt='logo'/>
+            </Link>
+            <nav>
+                <NavLink exact="true" activeclassname="active" to="/">
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="about-link" to="/user">
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+                </NavLink>
+            </nav>
+            <ul>
+                <li>
+                    <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/andrew-carter-046916252/'>
+                        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel='noreferrer' href='https://github.com/andcarter95'>
+                        <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
+                    </a>
+                </li>
+            </ul>
         </div>
             
     )
