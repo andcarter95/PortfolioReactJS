ddde800e7f2731fb6ab9074303fd24fe6906706e
andcarter95 <and.carter95@gmail.com>
2023-03-07 23:33:05 -0600

    Working on About page. Content and CSS built.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Routes, Route } from 'react-router-dom';
 // Components
 import Layout from './Components/Layout/layout';
 import Home from './Components/Home/home';
+import About from './Components/About/about';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Layout/>}>
         <Route index element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
       </Route>
     </Routes>
   );
---
+++ src/Components/About/about.js
@@ -0,0 +1,25 @@
+import './about.scss';
+
+const About = () => {
+
+    return (
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>About Me</h1>
+                <p>
+                    I'm an ambitious and motivated software developer looking for an
+                    opportunity to work on challenging projects for an established company.
+                </p>
+                <p>
+                    I have spent the last year learning the skills required to be a
+                    successful full stack developer at Kansas State University. Learning various 
+                    languages and platforms such as: HTML, CSS, React, JavaScript, PostgresSQL, 
+                    MongoDB, and Python. While spending my time outside of class taking courses 
+                    on Udemy in React and JavaScript.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About;
